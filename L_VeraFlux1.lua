4037be25127a1eeaf25a76917b384bfd13a9d5e2
Mike <mike.nye@xpanse.com.au>
2018-06-12 14:50:01 +0800

    Fix type error with trying tostring(response_body) in sendVeraFluxData

--- L_VeraFlux1.lua
+++ L_VeraFlux1.lua
@@ -257,6 +257,33 @@ local function readSettings(parentDevice)
 end
 
 
+-- Convert a lua table into a lua syntactically correct string (from gist: justnom/table_to_string.lua, with thanks)
+local function table_to_string(tbl)
+    local result = "{"
+    for k, v in pairs(tbl) do
+        -- Check the key type (ignore any numerical keys - assume its an array)
+        if type(k) == "string" then
+            result = result.."[\""..k.."\"]".."="
+        end
+
+        -- Check the value type
+        if type(v) == "table" then
+            result = result..table_to_string(v)
+        elseif type(v) == "boolean" then
+            result = result..tostring(v)
+        else
+            result = result.."\""..v.."\""
+        end
+        result = result..","
+    end
+    -- Remove leading commas from the result
+    if result ~= "" then
+        result = result:sub(1, result:len()-1)
+    end
+    return result.."}"
+end
+
+
 local function sendVeraFluxData(howCalled)
 	veraFluxDebugLog("sendVeraFluxData called via " .. howCalled)
 	veraFluxDebugLog("sendVeraFluxData: Payload length: " .. tostring(VERAFLUX_LINE_PROTOCOL:len()))
@@ -281,7 +308,7 @@ local function sendVeraFluxData(howCalled)
 	veraFluxDebugLog("InfluxDB server replied: " .. code)
 	
 	if tostring(code) == "400" then
-		veraFluxDebugLog("InfluxDB reply: " .. response_body)
+		veraFluxDebugLog("InfluxDB reply: " .. table_to_string(response_body))
 	end
 	
 	-- reset line protocol variable to avoid memory leak
